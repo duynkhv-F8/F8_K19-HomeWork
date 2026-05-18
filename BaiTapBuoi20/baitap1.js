9bcc3cd1f6867c6db3e425b4cbafcceec446c795
duynkhv-F8 <258801460+duynkhv-F8@users.noreply.github.com>
2026-05-18 17:02:35 +0700

    fix: update new code of session 20

--- BaiTapBuoi20/baitap1.js
+++ BaiTapBuoi20/baitap1.js
@@ -1,30 +1,17 @@
 const numbers = [9, 8, 3, 5, 6, 2, 7, 9];
 // Expected result: 8
 function solution(arr) {
-    let max = arr[0];
-    let answer = 0;
-
-    // Tìm giá trị ban đầu cho answer
-    for (let i = 0; i < arr.length; i++) {
-        if (arr[i] < max) {
-            answer = arr[i];
-            break;
+    let max = -Infinity;
+    let secondMax = -Infinity;
+    for (const number of arr) {
+        if (number > max) {
+            secondMax = max;
+            max = number;
+        }
+        else if (number < max && number > secondMax) {
+            secondMax = number;
         }
     }
-
-    // Tìm số lớn thứ 2
-    for (let i = 0; i < arr.length; i++) {
-
-        if (arr[i] > max) {
-            answer = max;
-            max = arr[i];
-        }
-
-        else if (arr[i] > answer && arr[i] < max) {
-            answer = arr[i];
-        }
-    }
-
-    return answer;
+    return secondMax;
 }
 console.log(solution(numbers));
